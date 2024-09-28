e7c50f3fe740adf4d64fed3ca2f58306a3bfc94b
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2024-09-28 17:16:10 +0900

    Make user inputs interactive

--- python/main.py
+++ python/main.py
@@ -3,13 +3,12 @@ sys.path.append('./src')
 import os
 from application import Application
 
-dirname  = os.path.join('..', 'json')
-if len(sys.argv) == 2:
-    _, filename = sys.argv
-else:
-    filename = ''
+dirname  = input('Provide the directory name you put the JSON file in: ')
+filename = input('Provide the filename of which JSON data name you would like to sort: ')
+order    = input('Provide asc(default) or desc you would like to sort key-value in: ')
+
 filepath = os.path.join(dirname, filename)
 
 print('Start exporting JSON data in {filepath}'.format(filepath = filepath))
-Application(dirname, filename).run()
+Application(dirname, filename, order).run()
 print('Done exporting JSON data in {filepath} 🎉'.format(filepath = filepath))
--- ruby/main.rb
+++ ruby/main.rb
@@ -1,10 +1,16 @@
 require_relative './src/application'
 
-dirname      = File.join('..', 'json')
-filename, *_ = ARGV
-filename     = filename || ''
-filepath     = File.join(dirname, filename)
+puts 'Provide the directory name you put the JSON file in'
+dirname = gets.chomp
+
+puts 'Provide the filename of which JSON data name you would like to sort'
+filename = gets.chomp
+
+puts 'Provide asc(default) or desc you would like to sort key-value in'
+order = gets.chomp
+
+filepath = File.join(dirname, filename)
 
 puts "Start exporting JSON data in #{filepath}"
-application  = ::JsonDataSorter::Application.run(dirname:, filename:)
+application  = ::JsonDataSorter::Application.run(dirname:, filename:, order:)
 puts "Done export JSON data in #{filepath} 🎉"
