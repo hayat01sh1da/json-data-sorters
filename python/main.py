6a67fbb72a5cb1fc9c9b92468d4ad521542cecc0
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2025-12-31 03:42:04 +0900

    Remove whitespace from command-line user interactive inputs

--- python/main.py
+++ python/main.py
@@ -5,9 +5,9 @@ import glob
 sys.path.append('./src')
 from application import Application
 
-dirname  = input('Provide the directory name you put the JSON file in: ')
-filename = input('Provide the filename of which JSON data name you would like to sort: ')
-order    = input('Provide asc(default) or desc you would like to sort key-value in: ')
+dirname  = input('Provide the directory name you put the JSON file in: ').strip()
+filename = input('Provide the filename of which JSON data name you would like to sort: ').strip()
+order    = input('Provide asc(default) or desc you would like to sort key-value in: ').strip()
 
 params = dict()
 for key, value in { 'dirname': dirname, 'filename': filename, 'order': order }.items():
--- ruby/main.rb
+++ ruby/main.rb
@@ -1,13 +1,13 @@
 require_relative './src/application'
 
 puts 'Provide the directory name you put the JSON file in:'
-dirname = gets.chomp
+dirname = gets.chomp.strip
 
 puts 'Provide the filename of which JSON data name you would like to sort:'
-filename = gets.chomp
+filename = gets.chomp.strip
 
 puts 'Provide asc(default) or desc you would like to sort key-value in:'
-order = gets.chomp
+order = gets.chomp.strip
 
 params = { dirname:, filename:, order: }.reject { |_, value| value.empty? }
 
