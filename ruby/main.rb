eb53723d11e6477d40a20e2412c6b150fa83fde2
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2026-04-17 03:10:07 +0900

    Fix: [error] Type `(::String | nil)` does not have method `chomp`

--- ruby/main.rb
+++ ruby/main.rb
@@ -3,13 +3,13 @@
 require_relative './src/application'
 
 puts 'Provide the directory name you put the JSON file in:'
-dirname = gets.chomp.strip
+dirname = gets&.chomp&.strip
 
 puts 'Provide the filename of which JSON data name you would like to sort:'
-filename = gets.chomp.strip
+filename = gets&.chomp&.strip
 
 puts 'Provide asc(default) or desc you would like to sort key-value in:'
-order = gets.chomp.strip
+order = gets&.chomp&.strip
 
 params = { dirname:, filename:, order: }.reject { |_, value| value.empty? }
 
